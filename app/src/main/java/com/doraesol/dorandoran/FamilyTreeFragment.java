18c906a6d4430f6c631c5b35396ddd1e128ca479
JJo <jjy456456@gmail.com>
2017-02-07 20:03:45 +0900

    핀치 줌 version 1.0

--- app/src/main/java/com/doraesol/dorandoran/FamilyTreeFragment.java
+++ app/src/main/java/com/doraesol/dorandoran/FamilyTreeFragment.java
@@ -5,20 +5,32 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import uk.co.senab.photoview.PhotoViewAttacher;
 
+public class FamilyTreeFragment extends Fragment
+{
+    ImageView bg;
+    PhotoViewAttacher mAttacher;
 
-/**
- * A simple {@link Fragment} subclass.
- */
-public class FamilyTreeFragment extends Fragment {
-    public FamilyTreeFragment() {
+    public FamilyTreeFragment()
+    {
         // Required empty public constructor
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+                             Bundle savedInstanceState)
+    {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_family_tree, container, false);
+        View viewView = inflater.inflate(R.layout.fragment_family_tree, container, false);
+        //ButterKnife.bind(this, viewView);
+
+        bg = (ImageView)viewView.findViewById(R.id.iv_fm_bg);
+        mAttacher = new PhotoViewAttacher(bg);
+
+        return viewView;
     }
 }
